590ce2620cfdbefbe8eeebcbfe428274a2213650
Debajit <debajitAIML@gmail.com>
2025-05-03 19:02:24 +0530

    title and subheader update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ list_pas2 = ['Literacy Rate','Households_with_Internet','sex ratio','Population'
 pas = sorted(data[list_pas])
 pas2 = sorted(data[list_pas2])
 
-st.sidebar.title('India Ka Data')
+st.sidebar.title('Indian Census (BETA)')
 state= st.sidebar.selectbox('select a state',list_state)
 primary = st.sidebar.selectbox('select primary',pas)
 secondary = st.sidebar.selectbox('select secondary',pas2)
@@ -23,16 +23,18 @@ plot = st.sidebar.button('Plot Graph')
 if plot:
 
     if state=='Overall India':
-        st.title('Size Represent Primary Parameter')
-        st.title('Color Represent Secondary Parameter')
+        st.subheader('V 1.0.1')
+        st.sidebar.write('- Size Represent Primary Parameter')
+        st.sidebar.write('- Color Represent Secondary Parameter')
 
         fig = px.scatter_mapbox(data, lat='Latitude', lon='Longitude',size = primary, color = secondary,
                                 hover_name='State',zoom=3.5,size_max=20,mapbox_style='carto-positron',
                                 width=1200, height=800, color_continuous_scale='hsv')
         st.plotly_chart(fig, use_container_width=True)
     else:
-        st.title('Size Represent Primary Parameter')
-        st.title('Color Represent Secondary Parameter')
+        st.subheader('V 1.0.1')
+        st.sidebar.write('- Size Represent Primary Parameter')
+        st.sidebar.write('- Color Represent Secondary Parameter')
 
         state_main = data[data['State'] == state]
 
